9aebf37d285ff129a64911c03cb30170cd689b9c
Jing Ran <jingran.mavis@gmail.com>
2016-07-17 16:02:37 -0400

    Update isEmail function

--- shipping-billing/script.js
+++ shipping-billing/script.js
@@ -80,32 +80,35 @@
 
     // Email validation
     validator.isEmail = function(email) {
-        var beforeAt,
-            afterAt,
-            splitBeforeAt,
-            splitAfterAt;
+        var local,
+        domain,
+        localSplit,
+        domainSplit;
 
         if (!email) return false;
+        if (email.indexOf(" ") !== -1) return false;
 
         if (email.indexOf("@") === -1 ||
             email.indexOf("@") !== email.lastIndexOf("@")) return false;
 
-        beforeAt = email.split("@")[0];
-        afterAt = email.split("@")[1];
-        splitBeforeAt = beforeAt.split(".");
-        splitAfterAt = afterAt.split(".");
+        local = email.split("@")[0];
+        domain = email.split("@")[1];
+        localSplit = local.split(".");
+        domainSplit = domain.split(".");
 
-        if (beforeAt === "" || beforeAt.indexOf(" ") >= 0) return false;
-        for (var i = 0; i < splitBeforeAt.length; i++) {
-            if (splitBeforeAt[i] === "" || splitBeforeAt[i] === " ")
+        if (beforeAt === "") return false;
+        for (var i = 0; i < beforeAtSplit.length; i++) {
+            if (beforeAtSplit[i] === "")
                 return false;
         }
-        if (afterAt === "" || afterAt.indexOf(" ") >=0) return false;
-        if (splitAfterAt.length < 2) return false;
-        for (var j = 0; j < splitAfterAt.length; j++) {
-            if (splitAfterAt[j] === "" || splitAfterAt[j] === " ")
-                return false;
-            if (splitAfterAt[splitAfterAt.length - 1].length < 2) return false;
+
+        if (domain === "") return false;
+        if (domain.indexOf("_") !== -1) return false;
+        if (domain.indexOf("-") === 0 || 
+            domain.lastIndexOf("-") === domain.length - 1) return false;
+        for (var j = 0; j < domainSplit.length; j++) {
+            if (domainSplit[j] === "") return false;
+            if (domainSplit[domainSplit.length - 1].length < 2) return false;
         }
 
         return true;
